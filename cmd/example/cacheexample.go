a21e0b8b5e80ae6cc8cba0e4acc7c32a71995f77
Simon <scgriffiths86@gmail.com>
2019-08-15 22:30:37 +0100

    Updated example to include self cleanup

--- cmd/example/cacheexample.go
+++ cmd/example/cacheexample.go
@@ -3,10 +3,11 @@ package main
 import (
 	gocache "GoCache"
 	"fmt"
+	"time"
 )
 
 var (
-	cache = gocache.New("test")
+	cache = gocache.New("test", 3)
 )
 
 func main() {
@@ -17,5 +18,9 @@ func main() {
 	cache.AddOrUpdate("key", "newvalue")
 	s, _ := cache.Fetch("key")
 	fmt.Println(s)
-	cache.Remove("key")
+	time.Sleep(5 * time.Second)
+	_, e := cache.Fetch("key")
+	if e != nil {
+		fmt.Println("cache item already removed from self cleanup")
+	}
 }
